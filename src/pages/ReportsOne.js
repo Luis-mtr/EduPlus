322975d9d367933a3f8302265c93eb04fb302912
Nem090 <102314405+Nem090@users.noreply.github.com>
2024-02-03 21:38:31 +0200

    Added Statistics page

--- src/pages/ReportsOne.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const ReportsOne = () => {
-  return (
-    <div className="reports">
-      <h1>ReportsOne</h1>
-    </div>
-  );
-};
--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+
+import { db } from "../firebase-config";
 
 const Statistics = () => {
+  const [questions, setQuestions] = useState([]);
+  const questionsCollectionRef = collection(db, "questions");
+
+  useEffect(() => {
+    const getQuestions = async () => {
+      const data = await getDocs(questionsCollectionRef);
+      setQuestions(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getQuestions();
+  }, []);
+
+  const distinctSubjects = [
+    ...new Set(questions.map((question) => question.subject)),
+  ];
+
+  const totalCountAsked = questions.reduce(
+    (total, question) => Number(total) + Number(question.countAsked),
+    0
+  );
+
+  const totalCountRight = questions.reduce(
+    (total, question) => Number(total) + Number(question.countRight),
+    0
+  );
+
+  const totalShowAAskQ = questions.reduce(
+    (total, question) =>
+      Number(total) +
+      Number(question.showAAskQ) / 2 +
+      Number(question.showQAskA) / 2,
+    0
+  );
+
   return (
     <div className="home">
       <h1>
@@ -11,20 +55,23 @@ const Statistics = () => {
 
       <p className="info">
         <br />
-        Bine ati venit la EduPlus
+        Statistici:
         <br /> <br />
-        Aceasta este o aplicație care vă ajută să rețineți informații prin
-        testare repetată
+        Sunt {questions.length} întrebări
         <br />
         <br />
-        Întrebările se vor repeta în funcție de corectitudinea răspunsurilor
-        date
+        Grupate în {distinctSubjects.length} materii
         <br />
         <br />
-        Puteți adăuga Întrebări în secțiunea{" "}
-        <Link to={{ pathname: `/AllQuestions` }}>'Toate Întrebările'</Link> și
-        le puteți încadra în{" "}
-        <Link to={{ pathname: `/Subjects` }}>Subiectele</Link> definite
+        Au fost date {totalCountAsked} de răspunsuri
+        <br />
+        <br />
+        Din care {Math.floor((totalCountRight * 100) / totalCountAsked)}%
+        corecte
+        <br />
+        <br />
+        Nivelul mediu de cunoaștere a întrebărilor este{" "}
+        {Math.floor(totalShowAAskQ / questions.length)}%
       </p>
     </div>
   );
